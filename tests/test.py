3b7d2f326cb096b32c682518905c97e91737ca16
alireza robatjazi <alireza19robatjazi@gmail.com>
2023-10-14 08:28:38 +0330

    README

--- tests/test.py
+++ tests/test.py
@@ -14,7 +14,19 @@ root.rowconfigure(1, weight = 1)
 root.rowconfigure(2, weight = 1)
 root.geometry("600x400")
 
-a = ["1","2"]
-print("2" in a)
+testcnvs = Frame(root)
+testcnvs.rowconfigure(0, weight=1)
+testcnvs.columnconfigure(0, weight=1)
+testcnvs.grid(sticky=NSEW, row = 0, column = 1, rowspan= 3)
+testcnvs.config(bg = "black")
+cnvs_lbl = Label(testcnvs, text = "اطلاعات", font="Arial 18 bold", bg = "black", fg = "white")
+cnvs_lbl.grid(row = 0, column= 0, sticky=NSEW)
+
+
+est = Label(root, text = "استعلام").grid(sticky=NSEW, row = 0, column= 0)
+vld = Label(root, text = "بررسی فایل").grid(sticky=NSEW, row = 1, column= 0)
+gzrsh = Label(root, text = "گزارشات").grid(sticky=NSEW, row = 2, column= 0)
+
+
 
 root.mainloop()
